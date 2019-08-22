1cc9cf656907f51bbae0fb20b7178bbd56f77774
Quasar Wei <quasar.wei@outlook.com>
2019-08-22 15:33:33 -0700

    added comments

--- index.js
+++ index.js
@@ -56,29 +56,30 @@ const STORE = {
   apiKey: 'JNPervKIi7fbQ2EeasBOwE1u8JX6qvhmmBGedtfZ',
   searchURL: 'https://developer.nps.gov/api/v1/parks',
   maxResults: 10
-
 };
 
+// generate all options for statecodes
 let generateStateOptions = (function() {
-
   let options = '';
   STORE.stateCodes.forEach( state => options +=`<option value="${state}">${state}</option>`);
   return {
     options};
 }());
 
+// send all options to DOM
 let displayDropDown = function() {
   console.log(generateStateOptions.options);
   $('select').html(generateStateOptions.options);
 };
 
-
+// format the query string
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
+// fetch function
 function getParks(query, maxResults=10) {
   const params = {
     stateCode: query,
@@ -104,7 +105,7 @@ function getParks(query, maxResults=10) {
     });  
 }
 
-
+// form drop down event listener - calls getParks when option is selected
 function selectedState() {
   $('form').on('change', e => {
     e.preventDefault();
@@ -114,6 +115,8 @@ function selectedState() {
   });
 
 }
+
+// display results as list items
 function displayResults(results){
   $('.search-results').empty();
   for(let i= 0; i <= 10 && i <= results.data.length; i++){
@@ -128,19 +131,8 @@ function displayResults(results){
       <p>${address.city}, ${address.stateCode}, ${address.postalCode}</p>
       </li>`
     );
-    
   }
 }
-
-// The user must be able to search for parks in one or more states.
-// The user must be able to set the max number of results, with a default of 10.
-// The search must trigger a call to NPS's API.
-// The parks in the given state must be displayed on the page. Include at least:
-//     Full name
-//     Description
-//     Website URL
-// The user must be able to make multiple searches and see only the results for the current search.
-// As a stretch goal, try adding the park's address to the results.
 
 function main() {
   displayDropDown();
